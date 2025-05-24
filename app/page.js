c5eb17e0bb7fac002d950f94e8363f4b712d3a62
Harsh-Srivastava <1by23ai058@bmsit.in>
2025-05-24 23:42:16 +0530

    dark/light mode

--- app/page.js
+++ app/page.js
@@ -1,11 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import DailyQuestion from "./components/question";
+import { useEffect, useState } from "react";
+import { SunMoon } from 'lucide-react';
 
 export default function Home() {
+  const images = ["/g23.png", "/g22.png"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [lightMode, setlightMode] = useState(false)
+
+  const switchImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+    setlightMode((prev)=>!prev)
+  };
+
+
+  useEffect(()=>{
+    document.body.classList.toggle("light",lightMode);
+  })
+
   return (
     <div className="home-container">
       <Image
-        src="/g23.png"
+        src={images[currentIndex]}
         alt="Background"
         fill
         className="background-image"
@@ -14,6 +34,7 @@ export default function Home() {
       <h1 className="title">OneDSA</h1>
       <h1 className="heading">One DSA Question a day</h1>
       <DailyQuestion />
+      <button onClick={switchImage} className="mode"><SunMoon/></button>
     </div>
   );
 }
